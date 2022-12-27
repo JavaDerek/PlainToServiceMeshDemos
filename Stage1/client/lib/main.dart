91f3e24ca96f9716c7a06ca14ede1d74813bd52b
Derek Ferguson <derek.ferguson@mail.ru>
2022-12-27 17:35:40 -0600

    Stage 2 server

--- Stage1/client/lib/main.dart
+++ Stage1/client/lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:async';
 import './greeting.dart';
 
 void main() {
-  http.read(Uri.parse('http://localhost:8080/greeting')).then((contents) {
-    Greeting g = Greeting.fromJson(jsonDecode(contents));
-    print(g.id);
-    print(g.content); // Here we output the contents of our service
-  });
+  const oneSec = Duration(seconds: 1);
+  Timer.periodic(
+      oneSec,
+      (Timer t) => http
+              .read(Uri.parse('http://localhost:8080/greeting'))
+              .then((contents) {
+            Greeting g = Greeting.fromJson(jsonDecode(contents));
+            print(g.id);
+            print(g.content); // Here we output the contents of our service
+          }));
 }
---
+++ Stage2/server/README.md
@@ -0,0 +1,9 @@
+Build Docker image with...
+
+sudo docker build -f Dockerfile -t greeter .
+
+Run Docker image with...
+
+sudo docker run greeter
+
+Kill running process with Ctrl-C
