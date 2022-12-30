e28c05f5aa49e82adea8b628c802364a93b67978
Derek Ferguson <derekmferguson@yahoo.co.uk>
2022-12-30 11:00:36 -0600

    Fixed Cyrillic

---
+++ Stage4/client/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+import './greeting.dart';
+
+void main() {
+  const oneSec = Duration(seconds: 1);
+  Timer.periodic(
+      oneSec,
+      (Timer t) => http
+              .read(Uri.parse('http://localhost:8080/greeting'))
+              .then((contents) {
+            var rawBytes = latin1.encode(contents);
+            String utf8Text = utf8.decode(rawBytes);
+            Greeting g = Greeting.fromJson(jsonDecode(utf8Text));
+            print(g.id);
+            print(g.content); // Here we output the contents of our service
+          }));
+}
