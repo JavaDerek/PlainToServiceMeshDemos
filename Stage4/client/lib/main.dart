deb36b8acb3b4e575d10ea0ce6615fca7e388e2d
Derek Ferguson <derekmferguson@yahoo.co.uk>
2022-12-30 12:35:54 -0600

    Clarified script names and added client header

--- Stage4/client/lib/main.dart
+++ Stage4/client/lib/main.dart
@@ -4,11 +4,15 @@ import 'dart:async';
 import './greeting.dart';
 
 void main() {
+  var headerMap = Map<String, String>();
+  headerMap["x-dark-launch"] = "v2";
   const oneSec = Duration(seconds: 1);
   Timer.periodic(
       oneSec,
-      (Timer t) =>
-          http.read(Uri.parse('http://server:8080/greeting')).then((contents) {
+      (Timer t) => http
+              .read(Uri.parse('http://localhost:8080/greeting'),
+                  headers: headerMap)
+              .then((contents) {
             var rawBytes = latin1.encode(contents);
             String utf8Text = utf8.decode(rawBytes);
             Greeting g = Greeting.fromJson(jsonDecode(utf8Text));
